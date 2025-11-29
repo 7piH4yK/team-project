a69548266ddb5e5a49de135a29aa32a962bd193e
xaderF <ryanyu365@gmail.com>
2025-11-28 20:42:27 -0500

    Fixing API stuff

--- src/main/java/data_access/api/TriviaResponse.java
+++ src/main/java/data_access/api/TriviaResponse.java
@@ -1,9 +1,13 @@
 package data_access.api;
 
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class TriviaResponse {
+
+    @SerializedName("response_code")
     private int responseCode;
+
     private List<Question> results;
 
     public int getResponseCode() {

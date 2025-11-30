d212e4b10cdd3a6af82637f3b608ece4471c7a58
adilbabri <adil.babri@gmail.com>
2025-11-30 14:56:05 -0500

    Merged pause menu to main

---
+++ src/main/java/interface_adapter/pause_menu/PauseMenuViewModel.java
@@ -0,0 +1,9 @@
+package interface_adapter.pause_menu;
+
+import interface_adapter.ViewModel;
+
+public class PauseMenuViewModel extends ViewModel {
+    public PauseMenuViewModel() {
+        super("pause_menu");
+    }
+}

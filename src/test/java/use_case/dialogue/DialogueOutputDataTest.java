e59f7dce1acad4ab72f90f2eef5bab3f758a001d
GrJr <daniil.grinchuk@mail.utoronto.ca>
2025-11-30 18:49:11 -0500

    Final tests for dialogue use case.

---
+++ src/test/java/use_case/dialogue/DialogueOutputDataTest.java
@@ -0,0 +1,24 @@
+package use_case.dialogue;
+
+import entity.DialogueBox;
+import entity.DialogueText;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class DialogueOutputDataTest {
+    @Test
+    public void testGetCurrentDialogue(){
+        DialogueBox NextBox = new DialogueBox(new DialogueText("NextBox",1,2), new ArrayList<>(),"bag.png");
+        DialogueOutputData DialogueOutputData = new DialogueOutputData();
+
+        assertNotSame(NextBox, DialogueOutputData.getCurrentDialogue());
+
+        DialogueOutputData.setCurrentDialogue(NextBox);
+
+        assertSame(NextBox, DialogueOutputData.getCurrentDialogue());
+    }
+}
